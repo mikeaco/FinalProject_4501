b6dcb6cb765fd0e5e6914045eb71d21f342a58d3
Syed Ahmad <sba5k@virginia.edu>
2022-12-06 10:22:55 -0500

    random additions

---
+++ mut_to_fasta.py
@@ -0,0 +1,34 @@
+import os
+import glob
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from Bio.SeqFeature import SeqFeature, FeatureLocation
+
+def seq_amino_loc(name):
+    amino_acid =""
+    # seq = ""
+    start_loc = 0
+    end_loc = 0
+    # date = ""
+    with open(name) as handle:
+        record = SeqIO.read(handle, "genbank")
+        date = record.annotations['date']
+        for j in record.features:
+            if j.type == "CDS":
+                start_loc = j.location.start
+                end_loc = j.location.end
+                amino_acid = j.qualifiers["translation"]
+        seq = record.seq[start_loc:end_loc]
+    return (seq, date, (start_loc, end_loc))
+
+full_list = []
+full_list.append("DENV-1_IND_826883_1982.gb")
+
+list_of_files2 = glob.glob("output2"+os.path.sep+"*.gb")
+list_of_files3 = glob.glob("output3"+os.path.sep+"*.gb")
+full_list += list_of_files2 + list_of_files3
+writer = open("all.fasta", 'w')
+for file in full_list:
+    seq, date, loc = seq_amino_loc(file)
+    writer.write(">"+file + " " + date + "\n"+str(seq)+"\n")
+
